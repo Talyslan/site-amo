42e0cdead1d9bd34324c0fdcc82d7691320ad1b3
Talyslan Canabarro <78499700+Talyslan@users.noreply.github.com>
2023-09-05 17:38:03 -0300

    reeinicio

--- src/partsOfPage/header/Header.jsx
+++ src/partsOfPage/header/Header.jsx
@@ -1,26 +1,26 @@
+import logoAmo from '../../../public/logoNav.png';
+import IconLogin from '../../../public/login.png';
 import HeaderStyles from './header.module.css';
-import Logo from '../../images/Logo.png';
-import IconLogin from '../../images/loginicon.png';
 
 export function Header() {
     return (
         <header className={HeaderStyles.header}>
             <nav className={HeaderStyles.nav}>
-                {/* Imagem da logo */}
-                <a href="../../">
-                    <img src={Logo} alt="Logo da AMO" 
-                        className={HeaderStyles.img}
-                    />
-                </a>
-
-                {/* Links do próprio site */}
-                <ul className={HeaderStyles.ul}>
-                    <li><a href="#oprojeto">O projeto</a></li>
-                    <li><a href="#inscreva-se">Inscreva-se</a></li>
-                    <li><a href="#equipe">Equipe</a></li>
-                    <li><a href="#contato">Contato</a></li>
-                </ul>
-                
+                <div className={HeaderStyles.logoBar}>
+                    {/* Imagem da logo */}
+                    <a href="#">
+                        <img src={logoAmo} alt="Logo da AMO"
+                            className={HeaderStyles.img}
+                        />
+                    </a>
+                    {/* Links do próprio site */}
+                    <ul className={HeaderStyles.ul}>
+                        <li><a href="#oprojeto">Treinamento</a></li>
+                        <li><a href="#inscreva-se">Notícias</a></li>
+                        <li><a href="#equipe">O Projeto</a></li>
+                    </ul>
+                    
+                </div>
                 {/* Links para a página de Login e Registro */}
                 <div className={HeaderStyles.loginRegister}>
                     <a href="#">
@@ -31,5 +31,5 @@ export function Header() {
                 </div>
             </nav>
         </header>
-    )
+    );
 }
