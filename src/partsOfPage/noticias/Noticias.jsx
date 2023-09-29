b114419fb7ead482cb7c6f724df1dad55ae7f210
Talyslan Canabarro <78499700+Talyslan@users.noreply.github.com>
2023-09-29 10:45:30 -0300

    add link das midias e mudando imgs

--- src/partsOfPage/noticias/Noticias.jsx
+++ src/partsOfPage/noticias/Noticias.jsx
@@ -12,19 +12,19 @@ import 'swiper/css/navigation'; //setinha de navegação
 import 'swiper/css/pagination'; //bolinhas
 import 'swiper/css/scrollbar';
 
-import n1 from '../../../public/carrossel/noticia5.png';
-import n2 from '../../../public/carrossel/noticia4.png';
-import n3 from '../../../public/carrossel/noticia3.png';
-import n4 from '../../../public/carrossel/noticia2.png';
-import n5 from '../../../public/carrossel/noticia1.png';
+import n1 from '../../../public/carrossel/noticia5.jpg';
+import n2 from '../../../public/carrossel/noticia4.jpg';
+import n3 from '../../../public/carrossel/noticia3.jpg';
+import n4 from '../../../public/carrossel/noticia2.jpg';
+import n5 from '../../../public/carrossel/noticia1.jpg';
 
 export function Noticias() {
     const imagens = [
-        { id: 1, image: n1 },
-        { id: 2, image: n2 },
-        { id: 3, image: n3 },
-        { id: 4, image: n4 },
-        { id: 5, image: n5 }
+        { id: 1, image: n1, url: '#' },
+        { id: 2, image: n2, url: '#' },
+        { id: 3, image: n3, url: '#' },
+        { id: 4, image: n4, url: '#' },
+        { id: 5, image: n5, url: '#' }
       ];
       
     const [slidePerView, setSlidesPerView] = useState(2);
@@ -54,11 +54,13 @@ export function Noticias() {
         <p>Descubra informações sobre o mundo da Informática.</p>
         </div>
 
-        <Swiper slidesPerView={slidePerView} pagination={{ clickable: true}} navigation>
+        <Swiper slidesPerView={slidePerView} pagination={{clickable: true}} navigation>
         {
             imagens.map( (item) => (
             <SwiperSlide key={item.id}>
-                <img src={item.image} alt="Imagem" className={NoticiaStyles.carrosselImg} />
+                <a href={item.url}>
+                    <img src={item.image} alt="Imagem" className={NoticiaStyles.carrosselImg} />
+                </a>
             </SwiperSlide>
             ))
         }
